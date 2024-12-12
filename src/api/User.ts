d9f3c8074b8fe8debf3310c09ffdf57034c2e038
Mark Enfermo <112786278+orenji-neko@users.noreply.github.com>
2024-12-13 02:31:15 +0800

    added read, update, delete for user

---
+++ src/api/User.ts
@@ -0,0 +1,128 @@
+import { Elysia, t } from "elysia";
+import Auth from "../auth";
+import { PrismaClient, Prisma, User } from "@prisma/client";
+
+const User = new Elysia({ prefix: "users" })
+  .decorate("prisma", new PrismaClient())
+  .use(Auth)
+  /**
+   * Get a list of users
+   */
+  .get("/", async ({ error, prisma }) => {
+
+    // if no id param
+    const users: User[] = await prisma.user.findMany();
+
+    return {
+      data: users
+    };
+
+  }, {
+    protected: true
+  })
+  /**
+   * Get details of a specific user
+   */
+  .get("/:id", async ({ error, prisma, params: { id } }) => {
+    
+    const user: User | null = await prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    });
+
+    if(!user) {
+      return error(404, {
+        message: "User not found"
+      })
+    }
+
+    return {
+      data: user
+    }
+
+  }, {
+    protected: true
+  })
+  /**
+   * Update details of a user
+   */
+  .put("/:id", async ({ error, body, params, prisma }) => {
+    const { id } =  params;
+    const { email, password, lastname, firstname, phone } = body;
+
+    try {
+      const user = await prisma.user.update({
+        where: {
+          id: id
+        },
+        data: {
+          email: email,
+          password: password,
+          lastname: lastname,
+          firstname: firstname,
+          phone: phone
+        }
+      });
+
+      return {
+        message: "User update success",
+        data: user
+      }
+    }
+    catch(err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          // Unique constraint failure
+          return error(409, {
+            message: "Update failed: Unique constraint violation",
+          });
+        }
+
+        if (err.code === 'P2025') {
+          // Record not found
+          return error(404, {
+            message: "User not found",
+          });
+        }
+
+        return error(500, {
+          message: "Failed to update user"
+        })
+      }
+    }
+
+  }, {
+    protected: true,
+    body: "register"
+  })
+  .delete("/:id", async ({ params: { id }, prisma, error }) => {
+    try {
+      const user = await prisma.user.delete({
+        where: {
+          id: id
+        }
+      });
+      return {
+        message: "User delete success",
+        data: user
+      }
+    }
+    catch(err) {
+      if(err instanceof Prisma.PrismaClientKnownRequestError) {
+        if(err.code === "P2025") {
+          return error(404, {
+            message: "User not found"
+          })
+        }
+
+        return error(500, {
+          message: "Failed to delete user"
+        })
+      }
+    }
+  }, {
+    protected: true
+  })
+
+export default User;
